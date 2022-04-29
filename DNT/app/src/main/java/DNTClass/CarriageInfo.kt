5aeb38d674f1183263177d3909e1cf9b9e203ad2
Yoon Jae Uk <sso9409@naver.com>
2022-04-29 11:10:00 +0900

    박영인
    ddwd

---
+++ DNT/app/src/main/java/DNTClass/CarriageInfo.kt
@@ -0,0 +1,14 @@
+package DNTClass
+
+// writer : Yoon Jae Uk
+// date : 2022.04.29 ~ ?
+// content : 객차 정보를 담고있는 클래스이다.
+
+/* class and member init declaration begin */
+class CarriageInfo() {
+
+/* class and member init declaration end */
+
+    /* class method declaration begin */
+    /* class method declaration end */
+}
