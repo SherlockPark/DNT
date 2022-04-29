96dee163ab236ec4b0e901d81fb953bb43bd8ae5
SherlockPark <78007848+SherlockPark@users.noreply.github.com>
2022-04-29 14:15:48 +0900

    dnt Android

---
+++ DNT/DNT/app/src/main/java/DNTClass/TrainInfo.kt
@@ -0,0 +1,15 @@
+package DNTClass
+
+// writer : Yoon Jae Uk
+// date : 2022.04.29 ~ ?
+// content : 기차 정보를 담고있는 클래스이다.
+
+/* class and member init declaration begin */
+class TrainInfo (){
+
+
+/* class and member init declaration end */
+
+    /* class method declaration begin */
+    /* class method declaration end */
+}
